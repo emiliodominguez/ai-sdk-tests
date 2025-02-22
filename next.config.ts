8babab9201cd955e41931c270c0565a1d1901f44
Emilio Dominguez <emilioadominguezb@gmail.com>
2025-02-22 20:04:24 -0300

    Added Sass options to silence deprecation warnings in Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 	reactStrictMode: false,
+	sassOptions: {
+		silenceDeprecations: ["legacy-js-api", "bogus-combinators"],
+	},
 };
 
 export default nextConfig;
